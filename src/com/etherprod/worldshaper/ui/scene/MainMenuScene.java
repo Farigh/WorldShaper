c2d641178469e4ca58380abf32053d4c2e4e06f8
GARCIN David <david.garcin.pro@gmail.com>
2013-03-02 23:33:16 +0100

    main_menu: added play and options buttons to the scene

--- src/com/etherprod/worldshaper/ui/scene/MainMenuScene.java
+++ src/com/etherprod/worldshaper/ui/scene/MainMenuScene.java
@@ -1,6 +1,11 @@
 package com.etherprod.worldshaper.ui.scene;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.entity.scene.menu.MenuScene;
+import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.andengine.entity.scene.menu.item.IMenuItem;
+import org.andengine.entity.scene.menu.item.SpriteMenuItem;
+import org.andengine.entity.scene.menu.item.decorator.ScaleMenuItemDecorator;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
@@ -17,9 +22,15 @@ import com.etherprod.worldshaper.MainActivity;
 import com.etherprod.worldshaper.ResourcesManager;
 import com.etherprod.worldshaper.ui.scene.SceneManager.SceneType;
 
-public class MainMenuScene extends MyScene
+public class MainMenuScene extends MyScene implements IOnMenuItemClickListener
 {
+	private MenuScene menuChildScene;
+	private final int MENU_PLAY = 0;
+	private final int MENU_OPTIONS = 1;
+	
 	private static ITextureRegion menu_background_region;
+	private static ITextureRegion play_region;
+	private static ITextureRegion options_region;
 	    
 	private static BuildableBitmapTextureAtlas menuTextureAtlas;
 	
@@ -28,9 +39,11 @@ public class MainMenuScene extends MyScene
 		MainActivity activity = ResourcesManager.getInstance().getActivity();	
 		
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("menu/");
-		menuTextureAtlas = new BuildableBitmapTextureAtlas(activity.getTextureManager(), 800, 480,
+		menuTextureAtlas = new BuildableBitmapTextureAtlas(activity.getTextureManager(), 1024, 1024,
 				TextureOptions.BILINEAR);
 		menu_background_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "main_menu_bg.png");
+		play_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "play_bt.png");
+		options_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "options_bt.png");
 
 		try 
 		{
@@ -40,7 +53,7 @@ public class MainMenuScene extends MyScene
 		} 
 		catch (final TextureAtlasBuilderException e)
 		{
-		        Debug.e(e);
+			Debug.e(e);
 		}
 	}
 	
@@ -48,7 +61,7 @@ public class MainMenuScene extends MyScene
 	public void createScene()
 	{
 		createBackground();
-		//createButtons();
+		createButtons();
 	}
 
 	@Override
@@ -69,6 +82,21 @@ public class MainMenuScene extends MyScene
 
 	}
 
+	@Override
+	public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
+			float pMenuItemLocalX, float pMenuItemLocalY)
+	{
+		switch(pMenuItem.getID())
+        {
+        	case MENU_PLAY:
+        		return true;
+        	case MENU_OPTIONS:
+        		return true;
+        	default:
+        		return false;
+        }
+	}
+
 	private void createBackground()
 	{
 		this.attachChild(new Sprite(0, 0, menu_background_region,
@@ -81,5 +109,30 @@ public class MainMenuScene extends MyScene
 	            pGLState.enableDither();
 	        }
 	    });	
+	}
+
+	private void createButtons()
+	{
+		menuChildScene = new MenuScene(activity.getCamera());
+	    menuChildScene.setPosition(0, 0);
+	    
+	    // zoom on click
+	    final IMenuItem playMenuItem = new ScaleMenuItemDecorator(new SpriteMenuItem(MENU_PLAY, 
+	    		play_region, activity.getVertexBufferObjectManager()), 1.2f, 1);
+	    final IMenuItem optionsMenuItem = new ScaleMenuItemDecorator(new SpriteMenuItem(MENU_OPTIONS, 
+	    		options_region, activity.getVertexBufferObjectManager()), 1.2f, 1);
+	    
+	    menuChildScene.addMenuItem(playMenuItem);
+	    menuChildScene.addMenuItem(optionsMenuItem);
+	    
+	    menuChildScene.buildAnimations();
+	    menuChildScene.setBackgroundEnabled(false);
+	    
+	    playMenuItem.setPosition(playMenuItem.getX(), playMenuItem.getY() + 26);
+	    optionsMenuItem.setPosition(optionsMenuItem.getX(), optionsMenuItem.getY() + 74);
+	    
+	    menuChildScene.setOnMenuItemClickListener(this);
+	    
+	    setChildScene(menuChildScene);
 	}
 }
